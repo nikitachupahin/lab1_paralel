f255389ed21d5bd60e7538658755faf71e4c55c3
Nikita Chupahin <nik123chupahin@gmail.com>
2025-02-05 11:57:51 +0200

    Tried to calculate the execution time

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <chrono>
+
+using std::chrono::nanoseconds;
+using std::chrono::duration_cast;
+using std::chrono::high_resolution_clock;
+
+void someFunction() {
+    int sum = 0;
+    for (int i = 0; i < 1000000000; ++i) {
+        sum += i;
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    auto start = high_resolution_clock::now();
+    someFunction();
+    auto end = high_resolution_clock::now();
+
+    auto duration = duration_cast<nanoseconds>(end - start);
+
+    std::cout << "Execution time: " << duration.count() * 1e-9 << " seconds" << std::endl;
     return 0;
 }
