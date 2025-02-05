c77945f205473c2e7108b26f7d7ca675acfee821
Nikita Chupahin <nik123chupahin@gmail.com>
2025-02-05 13:21:03 +0200

    Implemented task without parallelization

--- main.cpp
+++ main.cpp
@@ -1,24 +1,53 @@
 #include <iostream>
 #include <chrono>
+#include <cstdlib>
+#include <ctime>
 
-using std::chrono::nanoseconds;
-using std::chrono::duration_cast;
-using std::chrono::high_resolution_clock;
+using namespace std;
+using chrono::nanoseconds;
+using chrono::duration_cast;
+using chrono::high_resolution_clock;
 
-void someFunction() {
-    int sum = 0;
-    for (int i = 0; i < 1000000000; ++i) {
-        sum += i;
+void fillAndDisplayMatrix(int N) {
+    int matrix[N][N];
+
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            matrix[i][j] = rand() % 10;
+        }
+    }
+
+    cout << "Матриця:" << endl;
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "\nМатриця, відображена симетрично:" << endl;
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            cout << matrix[j][i] << " ";
+        }
+        cout << endl;
     }
 }
 
 int main() {
+    srand(time(NULL));
+
+    int n = 0;
+    cout << "Введи розмір матриці: ";
+    cin >> n;
+
     auto start = high_resolution_clock::now();
-    someFunction();
+    fillAndDisplayMatrix(n);
     auto end = high_resolution_clock::now();
 
     auto duration = duration_cast<nanoseconds>(end - start);
 
-    std::cout << "Execution time: " << duration.count() * 1e-9 << " seconds" << std::endl;
+    cout << "\nЧас виконання: " << duration.count() * 1e-6 << " мілісекунд" << endl;
+
     return 0;
 }
