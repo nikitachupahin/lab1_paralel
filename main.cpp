740d3bb65dd9aea139700cd3ca282288e7041793
Nikita Chupahin <nik123chupahin@gmail.com>
2025-02-05 21:16:08 +0200

    Implemented task with parallelization

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <thread>
 #include <chrono>
 #include <cstdlib>
 #include <ctime>
+#include <vector>
 
 using namespace std;
 using chrono::nanoseconds;
@@ -16,6 +18,13 @@ int** allocMatrix(int size) {
     return matrix;
 }
 
+void deleteMatrix(int** matrix, int size) {
+    for (int i = 0; i < size; i++) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+}
+
 void fillMatrix(int** matrix, int size) {
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
@@ -24,14 +33,36 @@ void fillMatrix(int** matrix, int size) {
     }
 }
 
-void deleteMatrix(int** matrix, int size) {
-    for (int i = 0; i < size; i++) {
-        delete[] matrix[i];
+void compute(int** matrix, int size, int start, int end) {
+    for (int i = start; i < end; i++) {
+        for (int j = i + 1; j < size; j++) {
+            swap(matrix[i][j], matrix[j][i]);
+        }
     }
-    delete[] matrix;
 }
 
-void compute(int** matrix, int size) {
+void computeParallel(int** matrix, int size, int num_threads) {
+    auto payload_begin = high_resolution_clock::now();
+    vector<thread> threads;
+    int segment_matrix = size / num_threads;
+    int start = 0;
+
+    for (int i = 0; i < num_threads; i++) {
+        int end = (i == num_threads - 1) ? size : start + segment_matrix;
+        threads.emplace_back(compute, matrix, size, start, end);
+        start = end;
+    }
+
+    for (auto& thread : threads) {
+        thread.join();
+    }
+
+    auto payload_end = high_resolution_clock::now();
+    auto elapsed = duration_cast<nanoseconds>(payload_end - payload_begin);
+    printf("Payload Time: %.3f seconds.\n", elapsed.count() * 1e-9);
+}
+
+void computeNotParallel(int** matrix, int size) {
     auto payload_begin = high_resolution_clock::now();
     for (int i = 0; i < size / 2; i++) {
         for (int j = i + 1; j < size; j++) {
@@ -50,11 +81,16 @@ int main() {
     cout << "Введіть розмір матриці: ";
     cin >> n;
 
+    int num_threads;
+    cout << "Введіть кількість потоків: ";
+    cin >> num_threads;
+
     int** matrix = allocMatrix(n);
 
     fillMatrix(matrix, n);
 
-    compute(matrix, n);
+//    computeNotParallel(matrix, n);
+    computeParallel(matrix, n, num_threads);
 
     deleteMatrix(matrix, n);
 
